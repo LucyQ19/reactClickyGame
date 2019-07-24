d029c6669136b0cfd2a2b2f5a81887cf75b23af5
LucyQ19 <lucyq19@gmail.com>
2019-07-24 17:01:36 -0500

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import Footer from './components/Footer/Footer';
-import MainContainer from './components/Container/MainContainer'
+import MainContainer from './components/Container/MainContainer';
 import './App.css';
 
 class App extends Component {
